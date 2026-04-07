8f76e5369d1f83b61d33f793261da6edd4587dcd
Paulyn <realjakeojeff@gmail.com>
2026-04-08 03:34:57 +0530

    Buttons along with highlighted event text

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
           <div>
             <h4 className="text-xl">simulate gravity, get gravity defied back on your desk</h4>
           </div>
-          <div>      {/* This is a JSX comment */}
-
+          <div className="flex items-center justify-center">      {/* Temporary placeholder */}
+            <h1 className="text-9xl">Orbit</h1>
           </div>
           <div className="flex items-center justify-center">
-            <a href="fillout.com">Submit</a>
+            <a className="bg-cyan-800/50 hover:bg-cyan-800 transition-all duration-200 ease-in-out rounded-2xl p-3 py-2 text-3xl" href="fillout.com">Submit</a>
           </div>
         </div>
       </main>
