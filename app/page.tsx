088afe6574215467d91369d075cd42fa6190f89b
Paulyn <realjakeojeff@gmail.com>
2026-04-08 19:26:19 +0530

    Added logo

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,8 @@ export default function Home() {
             <h4 className="text-xl">simulate gravity, get gravity defied back on your desk</h4>
           </div>
           <div className="flex items-center justify-center">      {/* Temporary placeholder */}
-            <h1 className="text-9xl">Orbit</h1>
+            {/* <h1 className="text-9xl">Orbit</h1> */}
+            <Image width={800} height={300} src="/logo.png"/>
           </div>
           <div className="flex items-center justify-center">
             <a className="bg-cyan-800/50 hover:bg-cyan-800 transition-all duration-200 ease-in-out rounded-2xl p-3 py-2 text-3xl" href="fillout.com">Submit</a>
