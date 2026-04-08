1d69ad2326900240473a97cfe31b374332ac2a5f
Paulyn <realjakeojeff@gmail.com>
2026-04-09 00:42:08 +0530

    Added Colors to items and elements

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col items-center justify-center min-h-screen w-full">
-        <div className="items-center justify-center">
-          <div>
-            <h4 className="text-xl">simulate gravity, get gravity defied back on your desk</h4>
+      <main className="bg-[#010C15] flex flex-col items-center justify-center min-h-screen w-full">
+        <div className=" items-center justify-center">
+          <div className="flex items-center justify-center">
+            <h4 className="text-xl text-[#EBDFCE]">simulate gravity, get gravity defied back on your desk</h4>
           </div>
           <div className="flex items-center justify-center">      {/* Temporary placeholder */}
             {/* <h1 className="text-9xl">Orbit</h1> */}
-            <Image width={800} height={300} src="/logo.png"/>
+            <Image alt="orbit-logo " width={800} height={300} src="/logo.png"/>
           </div>
           <div className="flex items-center justify-center">
-            <a className="bg-cyan-800/50 hover:bg-cyan-800 transition-all duration-200 ease-in-out rounded-2xl p-3 py-2 text-3xl" href="fillout.com">Submit</a>
+            <a className="bg-[#006898] hover:bg-[#204B65] transition-all duration-200 ease-in-out rounded-2xl p-3 py-2 text-3xl" href="fillout.com">Submit</a>
           </div>
         </div>
       </main>
