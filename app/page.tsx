7677e94d12ec04ca0f3892f519f565119e3e1b49
Paulyn <realjakeojeff@gmail.com>
2026-04-09 13:42:01 +0530

    Added Columns for the About Section

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,25 @@ export default function Home() {
           <div className="flex items-center justify-center">      {/* Temporary placeholder */}
             {/* <h1 className="text-9xl">Orbit</h1> */}
             <Image alt="orbit-logo" src="/logo.png" width={0} height={0} sizes="100vw" className="w-auto h-auto" />          </div>
-            <div className="flex flex-col items-center justify-center">
-              <a className="bg-[#006898] hover:bg-[#204B65] font-(family-name:--font-lora) transition-all duration-300 ease-in-out rounded-4xl p-6 py-4 text-3xl" href="fillout.com">Submit</a>
-              <a className="text-[#EBDFCE] text- xl my-3" href="#about">..learn more?</a>
+          <div className="flex flex-col items-center justify-center">
+            <a className="bg-[#006898] hover:bg-[#204B65] font-(family-name:--font-lora) transition-all duration-300 ease-in-out rounded-4xl p-6 py-4 text-3xl" href="fillout.com">Submit</a>
+            <a className="text-[#EBDFCE] text- xl my-3" href="#about">..learn more?</a>
 
-            </div>
+          </div>
         </div>
       </main>
       <section id="about" className="bg-[#204B65] flex flex-col items-center justify-center min-h-screen w-full">
-        
+        <div>
+
+        </div>
+        <div className="flex">
+          <div className="border">
+            yes
+          </div>
+          <div className="border">
+            yes
+          </div>
+        </div>
       </section>
     </>
 
