37f44fd3d294a1c1104d89c0621338e4ec2aaac1
Paulyn <realjakeojeff@gmail.com>
2026-04-10 00:29:50 +0530

    Fixing orbit issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,10 +33,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <nav className="absolute w-full z-20 bg-transparent">
           <div className="max-w-5xl mx-auto px-6 py-4 flex items-center justify-end">
             <div className="flex items-center gap-2">
-              <a href="/#about" className="border-[#006898] bg-transparent transition-all duration-300 hover:border-[#204B65] hover:bg-[#006898] border-2  p-4 py-2 text-2xl font-(family-name:--font-lora) rounded-4xl">
+              <a href="/#about" className="border-[#006898] text-white bg-transparent transition-all duration-300 hover:border-[#204B65] hover:bg-[#006898] border-2  p-4 py-2 text-2xl font-(family-name:--font-lora) rounded-4xl">
                 about
               </a>
-              <a href="/#rewards" className="border-[#006898] bg-transparent transition-all duration-300 hover:border-[#204B65] hover:bg-[#006898] border-2  p-4 py-2 text-2xl font-(family-name:--font-lora) rounded-4xl">
+              <a href="/#rewards" className="border-[#006898] text-white bg-transparent transition-all duration-300 hover:border-[#204B65] hover:bg-[#006898] border-2  p-4 py-2 text-2xl font-(family-name:--font-lora) rounded-4xl">
                 rewards
               </a>
 
