9aa52978ff5dd5990bf5e04fbcb7dcb5992da2ad
Paulyn <realjakeojeff@gmail.com>
2026-04-09 17:53:56 +0530

    Added prize tier

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
       <section id="about" className="bg-[#204B65] flex flex-col items-center justify-center min-h-screen w-full">
         <div className="mx-70 items-center justify-center flex flex-col">
           <h3 className="text-8xl font-(family-name:--font-baumans) font-bold text-white ">what is orbit?</h3>
-          <p className="text-2xl font-(family-name:--font-lora)">Orbit is a You Ship, We Ship where highschoolers can create a
+          <p className="text-2xl font-(family-name:--font-lora) text-center">Orbit is a You Ship, We Ship where highschoolers can create a
             software application, web application or a game which is a gravity
             or space simulation where physics is the core mechanic and drives everything.
             In return, they get a magnetic levitation globe shipped to them!</p>
@@ -78,19 +78,25 @@ export default function Home() {
       <section id="rewards" className="bg-[#EBDFCE] flex flex-col items-center justify-center min-h-screen w-full">
         <div className="mx-70 items-center justify-center flex flex-col">
           <h3 className="text-8xl text-[#CE1921] font-(family-name:--font-baumans) font-bold ">rewards!</h3>
-
+         <p className="text-2xl text-[#CE1921] font-(family-name:--font-lora) text-center">
+  Prizes are given out for creative and polished projects which hit the minimum time requirement. Really creative and innovative projects are heavily appreciated and are great for validating 
+  your work!</p>  
         </div>
         <div className="grid grid-cols-2 my-10 gap-10">
-          <div className="bg-[#63A7B2] p-20 py-30 rounded-4xl rotate-3 transition-all duration-300 hover:rotate-0 justify-center items-center flex flex-col">
+          <div className="bg-[#63A7B2] px-5 py-30 rounded-4xl rotate-3 transition-all duration-300 hover:rotate-0 justify-center items-center flex flex-col">
             <Image alt="globe-prize" src="/prize.png" width={300} height={300} />
-            <h3 className="text-6xl font-(family-name:--font-baumans) font-semibold">Levitating Globe</h3>
+            <h3 className="text-5xl font-(family-name:--font-baumans) font-semibold">Levitating Globe</h3>
+                        <h2 className="text-xl font-(family-name:--font-lora) text-[#">{`well polished tier`}</h2>
+
             <h2 className="text-xl font-(family-name:--font-lora) text-[#">{`>= 10 hackatime hours`}</h2>
 
           </div>
-          <div className="bg-[#63A7B2] p-20 py-30 rounded-4xl -rotate-3 transition-all duration-300 hover:rotate-0 justify-center items-center flex flex-col">
-            <Image alt="globe-prize" src="/prize.png" width={300} height={300} />
-            <h3 className="text-6xl font-(family-name:--font-baumans) font-semibold">Levitating Globe</h3>
-            <h2 className="text-xl font-(family-name:--font-lora) text-[#">{`>= 10 hackatime hours`}</h2>
+          <div className="bg-[#63A7B2] px-5 py-30 rounded-4xl -rotate-3 transition-all duration-300 hover:rotate-0 justify-center items-center flex flex-col">
+            <Image alt="globe-prize" src="/bonus.png" width={300} height={300} />
+            <h3 className="text-5xl font-(family-name:--font-baumans) font-semibold">Mechanical Fidget Toy</h3>
+                        <h2 className="text-xl font-(family-name:--font-lora) text-[#">{`Bonus Tier`}</h2>
+
+            <h2 className="text-xl font-(family-name:--font-lora) text-[#">{`>= 3 hackatime hours`}</h2>
 
           </div>
         </div>
