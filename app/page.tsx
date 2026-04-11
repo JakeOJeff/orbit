7a59836667ec241b84d466c9bc481c9164736006
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-04-11 06:51:23 +0530

    Fixing and auditing with new styles

--- app/page.tsx
+++ app/page.tsx
@@ -24,17 +24,15 @@ export default function Home() {
           </div>
           <div className="flex flex-col items-center justify-center">
             <h4 className="text-base sm:text-xl text-[#EBDFCE] mb-3 text-center">from june 4th till 29th, 2026</h4>
-            <a className="bg-[#006898] hover:bg-[#204B65] font-(family-name:--font-lora) transition-all duration-300 ease-in-out rounded-4xl p-6 py-4 text-xl sm:text-3xl" href="fillout.com">Submit</a>
+            <a className="bg-[#006898] hover:bg-[#004b6e] text-[#010C15] hover:text-[#368dd4] font-(family-name:--font-lora) transition-all duration-300 ease-in-out rounded-4xl p-6 py-4 text-xl sm:text-3xl" href="fillout.com">Submit</a>
             <a className="text-[#EBDFCE] text-base sm:text-xl my-3" href="#about">..learn more?</a>
           </div>
           <div className="absolute bottom-0 right-0 z-20">
             <Image
               src="/border-circles-1.png"
               alt="Hack Club"
-              width = {0}
-              height = {0}
-              sizes="100vw"
-              className="w-100 h-auto max-w-xs sm:max-w-md md:max-w-lg"
+              width = {200}
+              height = {200}
             />
           </div>
         </div>
@@ -141,38 +139,37 @@ export default function Home() {
         </div>
 
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 my-10 gap-2 w-full max-w-4xl">
-          <div className="border-4 transition-all duration-300 rounded-4xl p-6 sm:p-10 hover:border-[#EBDFCE]">
-            <h1 className="text-2xl text-[#EBDFCE] sm:text-3xl mb-4 font-(family-name:--font-baumans) font-semibold">whats the cost?</h1>
-            <p className="text-white font-(family-name:--font-lora)">it is completely free to participate and get prizes!</p>
-          </div>
-          <div className="border-4 transition-all duration-300 rounded-4xl p-6 sm:p-10 hover:border-[#EBDFCE]">
-            <h1 className="text-2xl text-[#EBDFCE] sm:text-3xl mb-4 font-(family-name:--font-baumans) font-semibold">what counts as a ship?</h1>
-            <p className="text-white font-(family-name:--font-lora)">your Ship should have a public github repo, a live playable link, downloadable binary, etc</p>
-          </div>
-          <div className="border-4 transition-all duration-300 rounded-4xl p-6 sm:p-10 hover:border-[#EBDFCE]">
-            <h1 className="text-2xl text-[#EBDFCE] sm:text-3xl mb-4 font-(family-name:--font-baumans) font-semibold">who can participate?</h1>
-            <p className="text-white font-(family-name:--font-lora)">students and teenagers aged 13-18 can participate!</p>
-          </div>
-          <div className="border-4 transition-all duration-300 rounded-4xl p-6 sm:p-10 hover:border-[#EBDFCE]">
-            <h1 className="text-2xl text-[#EBDFCE] sm:text-3xl mb-4 font-(family-name:--font-baumans) font-semibold">what can i make?</h1>
-            <p className="text-white font-(family-name:--font-lora)">you can make any project that you like as long as gravity is the core mechanic, cc: <a className="underline hover:text-[#EBDFCE] transition-all duration-300" href="/#about">examples</a></p>
-          </div>
-          <div className="border-4 transition-all duration-300 rounded-4xl p-6 sm:p-10 hover:border-[#EBDFCE]">
-            <h1 className="text-2xl text-[#EBDFCE] sm:text-3xl mb-4 font-(family-name:--font-baumans) font-semibold">how to get the bonus prize?</h1>
-            <p className="text-white font-(family-name:--font-lora)">work for an extra three hours and get the bonus prize {`(and maaaaybe a sticker too)`}</p>
-          </div>
-          <div className="border-4 transition-all duration-300 rounded-4xl p-6 sm:p-10 hover:border-[#EBDFCE]">
-            <h1 className="text-2xl text-[#EBDFCE] sm:text-3xl mb-4 font-(family-name:--font-baumans) font-semibold">how do i track hours?</h1>
-            <p className="text-white font-(family-name:--font-lora)">you can track hours in your IDE using <a className="underline hover:text-[#EBDFCE] duration-300 transition-all" href="https://hackatime.hackclub.com">hackatime</a></p>
-          </div>
-
-
+      <div className="grid grid-cols-2 sm:grid-cols-3 my-10 gap-2 w-full max-w-4xl">
+        <div className="border-4 transition-all duration-300 rounded-4xl p-6 sm:p-10 hover:border-[#EBDFCE] hover:-translate-y-2 hover:[box-shadow:0_8px_30px_rgba(235,223,206,0.15)]">
+          <h1 className="text-2xl text-[#EBDFCE] sm:text-3xl mb-4 font-(family-name:--font-baumans) font-semibold">whats the cost?</h1>
+          <p className="text-white font-(family-name:--font-lora)">it is completely free to participate and get prizes!</p>
         </div>
+        <div className="border-4 transition-all duration-300 rounded-4xl p-6 sm:p-10 hover:border-[#EBDFCE] hover:-translate-y-2 hover:[box-shadow:0_8px_30px_rgba(235,223,206,0.15)]">
+          <h1 className="text-2xl text-[#EBDFCE] sm:text-3xl mb-4 font-(family-name:--font-baumans) font-semibold">what counts as a ship?</h1>
+          <p className="text-white font-(family-name:--font-lora)">your Ship should have a public github repo, a live playable link, downloadable binary, etc</p>
+        </div>
+        <div className="border-4 transition-all duration-300 rounded-4xl p-6 sm:p-10 hover:border-[#EBDFCE] hover:-translate-y-2 hover:[box-shadow:0_8px_30px_rgba(235,223,206,0.15)]">
+          <h1 className="text-2xl text-[#EBDFCE] sm:text-3xl mb-4 font-(family-name:--font-baumans) font-semibold">who can participate?</h1>
+          <p className="text-white font-(family-name:--font-lora)">students and teenagers aged 13-18 can participate!</p>
+        </div>
+        <div className="border-4 transition-all duration-300 rounded-4xl p-6 sm:p-10 hover:border-[#EBDFCE] hover:-translate-y-2 hover:[box-shadow:0_8px_30px_rgba(235,223,206,0.15)]">
+          <h1 className="text-2xl text-[#EBDFCE] sm:text-3xl mb-4 font-(family-name:--font-baumans) font-semibold">what can i make?</h1>
+          <p className="text-white font-(family-name:--font-lora)">you can make any project that you like as long as gravity is the core mechanic, cc: <a className="underline hover:text-[#EBDFCE] transition-all duration-300" href="/#about">examples</a></p>
+        </div>
+        <div className="border-4 transition-all duration-300 rounded-4xl p-6 sm:p-10 hover:border-[#EBDFCE] hover:-translate-y-2 hover:[box-shadow:0_8px_30px_rgba(235,223,206,0.15)]">
+          <h1 className="text-2xl text-[#EBDFCE] sm:text-3xl mb-4 font-(family-name:--font-baumans) font-semibold">how to get the bonus prize?</h1>
+          <p className="text-white font-(family-name:--font-lora)">work for an extra three hours and get the bonus prize {`(and maaaaybe a sticker too)`}</p>
+        </div>
+        <div className="border-4 transition-all duration-300 rounded-4xl p-6 sm:p-10 hover:border-[#EBDFCE] hover:-translate-y-2 hover:[box-shadow:0_8px_30px_rgba(235,223,206,0.15)]">
+          <h1 className="text-2xl text-[#EBDFCE] sm:text-3xl mb-4 font-(family-name:--font-baumans) font-semibold">how do i track hours?</h1>
+          <p className="text-white font-(family-name:--font-lora)">you can track hours in your IDE using <a className="underline hover:text-[#EBDFCE] duration-300 transition-all" href="https://hackatime.hackclub.com">hackatime</a></p>
+        </div>
+      </div>
+
       </section>
 
-      <div className="w-full h-[50px] bg-[#EBDFCE] items-center justify-center text-center">
-        <h4 className="font-(family-name:--font-baumans) text-[#010C15]">made with love by <a>JakeOJeff</a></h4>
+      <div className="w-full h-[50px] bg-[#EBDFCE] items-center justify-center text-center flex flex-col gap-0">
+        <h4 className="font-(family-name:--font-baumans) text-[#010C15]">made with love by <a>JakeOJeff</a> </h4>
         <h4 className="font-(family-name:--font-baumans) text-[#010C15]">fellowship '26 | 80's nasa <a href="https://archive.org/details/C-1984-5793" className="underline hover:text-[#204B65] transition-all duration-300" >program</a> palette</h4>
       </div>
 
